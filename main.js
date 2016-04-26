4f2581cc5cb7300a173fefa4443310a33bafca2e
kokushin <mail@kokush.in>
2016-04-26 15:55:54 +0900

    initialize commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var Vue = require('vue');
+var marked = require('marked');
+
+var mdve = new Vue({
+  el: '#mdve',
+  data: {
+    input: '# MarkDown Visual Editor'
+  },
+  filters: {
+    marked: marked
+  }
+});
